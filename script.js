87936197b43bab902a51aa737e0ac5acb4b07a8f
Ly Nguyen <Ly900@aol.com>
2015-11-26 23:29:37 -0500

    refactor search form and movieSearch into separate functions to pass along variables in callbacks

--- script.js
+++ script.js
@@ -1,36 +1,51 @@
 $(document).ready(function() {
+
   $("#movie_options").hide();
 
+
+  // Search Form and Button:
   $("#submit_button").on("click", function(evt) {
     evt.preventDefault();
-    $("#movie_options").show();
     var movie_name = $("#movie_name").val();
+    // clears input field
+    $("#movie_name").val("");
+    movieSearch(movie_name);
+  }) // ends on method
+
+
+  // Search for Movie using movie_name
+  function movieSearch(movie_name) {
     var url = "http://www.omdbapi.com/?s=" + movie_name;
     $.ajax({
       url: url,
       type: "get",
       dataType: "json"
-    }).done(function(response){
-      var movie_name = $("#movie_name").val();
-      var all_movies = response.Search;
-      console.log(movie_name)
-      console.log(url)
+    })
+    .done(function(response){
+      movieFound(movie_name, response);
+    })
+    .fail(function(){
+    }).always(function(){
+    })
+  } // Ends findMovie
 
+
+  // 
+  function movieFound(movie_name, response) {
+    $("#movie_options").show();
+    var all_movies = response.Search;
+    console.log(all_movies);
+  }
       // lists the movie titles from the response object as option elements in the select element created earlier
       for (var i=0; i < all_movies.length; i++) {
         $("#movie_options").append("<option><a href='google.com'>" + all_movies[i].Title +   "</a></option>");
         var movie_name = $("#movie_name").val();
       }
 
-      // clears input field
-      $("#movie_name").val("");
+      // Shows options
 
 
 
-    }).fail(function(){
-    }).always(function(){
-    })
 
-  }) // ends on method
 
 }) //ends ready method
