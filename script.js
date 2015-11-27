761139217d8e9c3ba3ed66efe73083c46bee40ba
Ly Nguyen <Ly900@aol.com>
2015-11-26 19:43:01 -0500

    movie object is received as an array

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 $(document).ready(function() {
 
-$("#submit_button").on("click", function(evt) {
-  evt.preventDefault();
-  var movie_name = $("#movie_name").val();
-  var url = "http://www.omdbapi.com/?s=" + movie_name;
-console.log(url);
+  $("#submit_button").on("click", function(evt) {
+    evt.preventDefault();
+    var movie_name = $("#movie_name").val();
+    var url = "http://www.omdbapi.com/?s=" + movie_name;
+    console.log(url);
+    $.ajax({
+      url: url,
+      type: "get",
+      dataType: "json"
+    }).done(function(response){
+      console.log(response);
+      
+    }).fail(function(){
+        console.log("Ajax request fails!")
+    }).always(function(){
+        console.log("Ajax request sent.")
+    })
 
-
-
-
-
-
-
-})
-
+  }) // ends on method
 
 }) //ends ready method
