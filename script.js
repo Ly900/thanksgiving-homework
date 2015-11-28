a989e3d577aeec664d71871e2ea282c557d8234f
Ly Nguyen <Ly900@aol.com>
2015-11-28 14:28:25 -0500

    change appends to inner html and hide old title and poster when a new movie is chosen

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ $(document).ready(function() {
     var movie_name = $("#movie_name").val();
     // clears input field
     $("#movie_name").val("");
+    $("h1#movie_title, #movie_poster_div").hide();
+
     movieSearch(movie_name);
   }) // ends on method
 
@@ -32,11 +34,11 @@ $(document).ready(function() {
 
   // Use movie_name and ajax response object to list out movie titles
   function moviesFound(movie_name, response) {
-    $("#movie_options").append("<option>Movies matching " + "\"" + movie_name + "\"" + "</option>");
+    var options = "<option>Movies matching " + "\"" + movie_name + "\"" + "</option>";
     var all_movies = response.Search;
     for (var i=0; i < all_movies.length; i++) {
       var movie = all_movies[i];
-      $("#movie_options").append("<option value='" + movie.imdbID + "'>" + movie.Title + "</option>");
+      $("#movie_options").html(options += "<option value='" + movie.imdbID + "'>" + movie.Title + "</option>");
     }
     $("#movie_options").show();
     showInfo();
@@ -44,9 +46,8 @@ $(document).ready(function() {
 
 
   // Shows the movie's title and poster picture
-  function showInfo(movie_name, response){
+  function showInfo(){
     $("#movie_options").on("change", function() {
-      console.log(this.value);
       var url = "http://www.omdbapi.com/?i=" + this.value;
       $.ajax({
         url: url,
@@ -56,15 +57,12 @@ $(document).ready(function() {
       .done(function(response){
         $("#movie_title_div").html(response.Title);
         $("#movie_poster_div img").attr("src", response.Poster);
-        $("#movie_poster_div").show();
+        $("h1#movie_title, #movie_poster_div").show();
       })
       .fail(function(){
-      }).always(function(){ 
+      }).always(function(){
       }) //ends ajax request
     }) //ends on method
-
-
   } // ends showInfo
-
 
 }) //ends ready method
